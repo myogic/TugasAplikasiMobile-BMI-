6848499db5f69dcd30f5bbbc6e299ecd3d5b8695
yogi <moh.yogi10065@gmail.com>
2019-10-02 18:21:06 +0700

    2

---
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -0,0 +1,14 @@
+git remote add origin https://github.com/myogic/TugasAplikasiMobile-BMI-.gitpackage com.example.bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
